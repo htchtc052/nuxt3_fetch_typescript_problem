7546285bed4618217d206af652f4b4968eb2abad
alekslog <htchtc052@gmail.com>
2024-01-03 18:06:32 +0300

    initial commit

---
+++ pages/test.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+}
+
+const { data: posts, pending } = await useAsyncData<Post[]>(
+"posts",
+() => $fetch("https://jsonplaceholder.typicode.com/posts"),
+{
+default: (): Post[] => [],
+}
+);
+
+console.debug(posts.value);
+</script>
+<template>
+  <h1>Account</h1>
+  <div>User pages</div>
+  <div v-if="pending">Loading...</div>
+  <div v-else>
+    <ul>
+      <li v-for="post of posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+
+  </div>
+</template>
+
+<style scoped></style>
